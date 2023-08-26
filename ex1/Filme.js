4a3d75569400f5d9cccf26ff5772eb394393ecb5
Lucas <lucas.f@gec.inatel.br>
2023-08-25 21:55:09 -0300

    ex2

---
+++ ex1/Filme.js
@@ -0,0 +1,55 @@
+class Filme {
+    constructor(titulo, ano, genero, duracao) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.assistido = false;
+        this.avaliacao = null;
+    }
+
+    exibirDetalhes() {
+        console.log(`Título: ${this.titulo}`);
+        console.log(`Ano: ${this.ano}`);
+        console.log(`Gênero: ${this.genero}`);
+        console.log(`Duração: ${this.duracao} minutos`);
+        console.log(`Assistido: ${this.assistido ? 'Sim' : 'Não'}`);
+        console.log(`Avaliação: ${this.avaliacao !== null ? this.avaliacao : 'Não avaliado'}`);
+    }
+
+    marcarComoAssistido() {
+        this.assistido = true;
+    }
+
+    avaliar(nota) {
+        if (nota >= 0 && nota <= 10) {
+            this.avaliacao = nota;
+        } else {
+            console.log("Nota inválida. A avaliação deve ser entre 0 e 10.");
+        }
+    }
+}
+
+module.exports = Filme; // Exporta a classe Filme para que ela possa ser usada em outros arquivos
+
+
+// const filme1 = new Filme("InterEstrelar", 2010, "Ficção Científica", 148);
+// const filme2 = new Filme("tres e demais", 1972, "Drama", 175);
+// const filme3 = new Filme("Toy Story", 1995, "Animação", 81);
+
+// filme1.exibirDetalhes();
+// console.log('-------------------');
+// filme2.exibirDetalhes();
+// console.log('-------------------');
+// filme3.exibirDetalhes();
+
+// filme1.marcarComoAssistido();
+// filme1.avaliar(9);
+
+// filme2.marcarComoAssistido();
+// filme2.avaliar(10);
+
+// console.log('-------------------');
+// filme1.exibirDetalhes();
+// console.log('-------------------');
+// filme2.exibirDetalhes();
---
+++ ex1/main.js
@@ -0,0 +1,79 @@
+const Filme = require('./Filme'); // Importa a classe Filme
+const prompt = require('prompt-sync')({ sigint: true }); // Se você estiver usando prompt-sync
+
+
+const filmes = [];
+
+function menu() {
+    console.log("\nMenu:");
+    console.log("1 - Adicionar um novo filme");
+    console.log("2 - Marcar um filme como assistido");
+    console.log("3 - Avaliar um filme");
+    console.log("4 - Exibir a lista de filmes");
+    console.log("5 - Sair do programa");
+
+    const escolha = parseInt(prompt("Escolha uma opção:"));
+
+    switch (escolha) {
+        case 1:
+            const titulo = prompt("Digite o título do filme:");
+            const ano = parseInt(prompt("Digite o ano do filme:"));
+            const genero = prompt("Digite o gênero do filme:");
+            const duracao = parseInt(prompt("Digite a duração do filme em minutos:"));
+            const filme = new Filme(titulo, ano, genero, duracao);
+            filmes.push(filme);
+            break;
+        case 2:
+            if (filmes.length === 0) {
+                console.log("Nenhum filme disponível para marcar como assistido.");
+            } else {
+                filmes.forEach((filme, index) => {
+                    console.log(`${index + 1} - ${filme.titulo}`);
+                });
+                const index = parseInt(prompt("Escolha o número do filme que você assistiu:")) - 1;
+                if (filmes[index]) {
+                    filmes[index].marcarComoAssistido();
+                    console.log("Filme marcado como assistido.");
+                } else {
+                    console.log("Número inválido.");
+                }
+            }
+            break;
+        case 3:
+            if (filmes.length === 0) {
+                console.log("Nenhum filme disponível para avaliar.");
+            } else {
+                filmes.forEach((filme, index) => {
+                    console.log(`${index + 1} - ${filme.titulo}`);
+                });
+                const index = parseInt(prompt("Escolha o número do filme que você quer avaliar:")) - 1;
+                if (filmes[index]) {
+                    const nota = parseFloat(prompt("Digite sua avaliação (0 a 10):"));
+                    filmes[index].avaliar(nota);
+                    console.log("Filme avaliado.");
+                } else {
+                    console.log("Número inválido.");
+                }
+            }
+            break;
+        case 4:
+            if (filmes.length === 0) {
+                console.log("Nenhum filme disponível.");
+            } else {
+                filmes.forEach((filme) => {
+                    filme.exibirDetalhes();
+                    console.log('-------------------');
+                });
+            }
+            break;
+        case 5:
+            console.log("Saindo do programa.");
+            return;
+        default:
+            console.log("Opção inválida.");
+    }
+
+    menu();
+}
+
+menu();
