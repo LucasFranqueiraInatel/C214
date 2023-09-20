3ceb5532442ea6d4ae4e16f9182873c3d69d089c
Joao Pedro <moreirajp12@hotmail.com>
2023-09-19 22:08:23 -0300

    Estruturação dos testes

---
+++ trabalhado1_testes_unitarios/GerenciadorTarefas.test.js
@@ -0,0 +1,23 @@
+const {expect} = require('chai');
+describe('Testes gerenciador de tarefas',() =>{
+    it('Teste mudança de status', () =>{
+
+
+    })
+
+    it('Teste visualizar tarefas', () =>{
+        
+    })
+
+    it('Teste adicionar tarefa', () =>{
+        
+    })
+
+    it('Teste excluir tarefa', () =>{
+        
+    })
+
+
+
+
+})
