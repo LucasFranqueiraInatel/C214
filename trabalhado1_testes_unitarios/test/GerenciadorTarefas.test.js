234f90b63e57cf441dce20483329765cdac965bf
Lucas <lucas.f@gec.inatel.br>
2023-09-24 17:50:06 -0300

    final

--- trabalhado1_testes_unitarios/test/GerenciadorTarefas.test.js
+++ trabalhado1_testes_unitarios/test/GerenciadorTarefas.test.js
@@ -1,47 +1,42 @@
-const {expect} = require('chai');
-const {
-  exibirMenu,
-  adicionarTarefa,
-  visualizarTarefas,
-  atualizarStatus,
-  excluirTarefa,
-} = require('../gerenciadorTarefas');
+const { expect } = require('chai');
+const { Task, TaskManager } = require('../src/taskManager');
 
-describe ('Testes gerenciador de tarefas',() =>{
+describe('Gerenciador de Tarefas', () => {
 
-  it('deve exibir uma lista de tarefas com o título, descrição e status', () => {
-    // Criar algumas tarefas
-    tarefas.push({
-      tiulo: 'Tarefa 1',
-      descricao: 'Descrição da tarefa 1',
-      status: 'A fazer',
-    });
-    tarefas.push({
-      tiulo: 'Tarefa 2',
-      descricao: 'Descrição da tarefa 2',
-      status: 'Em andamento',
-    });
-    tarefas.push({
-      tiulo: 'Tarefa 3',
-      descricao: 'Descrição da tarefa 3',
-      status: 'Concluída',
-    });
-  
-    // Executar a função
-    visualizarTarefas();
-  
-    // Assertivas
-    expect(console.log).toHaveBeenCalledWith(
-      chalk.blue('------------------------------'),
-      chalk.bold('1. Tarefa 1 - Descrição da tarefa 1 - A fazer'),
-      chalk.bold('2. Tarefa 2 - Descrição da tarefa 2 - Em andamento'),
-      chalk.bold('3. Tarefa 3 - Descrição da tarefa 3 - Concluída'),
-      chalk.blue('------------------------------')
-    );
+  it('Deve adicionar uma nova tarefa', () => {
+    const taskManager = new TaskManager();
+    taskManager.addTask('Comprar mantimentos', 'Leite, ovos, pão', 'Pendente');
+    const tasks = taskManager.viewTasks();
+    expect(tasks).to.have.lengthOf(1);
+    expect(tasks[0]).to.be.an.instanceOf(Task);
+    expect(tasks[0].title).to.equal('Comprar mantimentos');
   });
 
-   
+  it('Deve visualizar a lista de tarefas', () => {
+    const taskManager = new TaskManager();
+    taskManager.addTask('Comprar mantimentos', 'Leite, ovos, pão', 'Pendente');
+    taskManager.addTask('Estudar JavaScript', 'Capítulo 5', 'Em progresso');
+    const tasks = taskManager.viewTasks();
+    expect(tasks).to.have.lengthOf(2);
+    expect(tasks[1].description).to.equal('Capítulo 5');
+  });
 
+  it('Deve atualizar o status de uma tarefa', () => {
+    const taskManager = new TaskManager();
+    taskManager.addTask('Comprar mantimentos', 'Leite, ovos, pão', 'Pendente');
+    taskManager.updateTaskStatus(0, 'Concluído');
+    const tasks = taskManager.viewTasks();
+    expect(tasks[0].status).to.equal('Concluído');
+  });
 
+  it('Deve excluir uma tarefa', () => {
+    const taskManager = new TaskManager();
+    taskManager.addTask('Comprar mantimentos', 'Leite, ovos, pão', 'Pendente');
+    taskManager.addTask('Estudar JavaScript', 'Capítulo 5', 'Em progresso');
+    taskManager.deleteTask(0);
+    const tasks = taskManager.viewTasks();
+    expect(tasks).to.have.lengthOf(1);
+    expect(tasks[0].title).to.equal('Estudar JavaScript');
+  });
 
-})
+});
